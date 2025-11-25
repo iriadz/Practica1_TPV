07cb5119460e1db63198d7db5d1a656fe5bacd29
Cicl0 <alejag57@ucm.es>
2025-11-25 17:23:10 +0100

    fix a

--- src/GameState.h
+++ src/GameState.h
@@ -2,13 +2,11 @@
 #include <list>
 #include "GameObject.h"
 #include "EventHandler.h"
-#include
 class GameState
 {
 private:
 	std::list<GameObject*> objects;
 	std::list<EventHandler*> events;
-	std::list<DelayedCallback> 
 	Game* game;
 };
 
