4526ad585f973f5003392bddf2bd48da9830f25f
Cicl0 <alejag57@ucm.es>
2025-11-25 17:22:08 +0100

    a

---
+++ src/GameState.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <list>
+#include "GameObject.h"
+#include "EventHandler.h"
+#include
+class GameState
+{
+private:
+	std::list<GameObject*> objects;
+	std::list<EventHandler*> events;
+	std::list<DelayedCallback> 
+	Game* game;
+};
+
