bbfed2d5cd782bf6a09725beaaad11f6449684a1
Cicl0 <alejag57@ucm.es>
2025-11-25 17:37:08 +0100

    update PlayState

--- src/PlayState.h
+++ src/PlayState.h
@@ -1,5 +1,16 @@
 ﻿#pragma once
+#include <list>
+#include "GameObject.h"
+#include "SceneObject.h"
 class PlayState
 {
+public:
+	PlayState(std::list<GameObject*> o, std::list<SceneObject*> s) :
+		objects(o),
+		sceneObjects(s)
+	{ }
+private:
+	std::list<GameObject*> objects;
+	std::list<SceneObject*> sceneObjects;
 };
 
