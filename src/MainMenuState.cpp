8e031ba92cdf9830579e2d71ea67ae0de3e6ec1a
iriadz <idocam01@ucm.es>
2025-11-29 13:19:42 +0100

    Update MainMenuState.cpp

---
+++ src/MainMenuState.cpp
@@ -0,0 +1,37 @@
+﻿#include "MainMenuState.h"
+#include "Game.h"
+#include "Button.h"
+#include "Label.h"
+#include <filesystem>
+#include <iostream>
+
+MainMenuState::MainMenuState(Game* game) : GameState(game), selected(0) {
+    // discover maps
+    namespace fs = std::filesystem;
+    try {
+        for (auto& entry : fs::directory_iterator("assets/maps")) {
+            mapNames.push_back(entry.path().stem().string());
+        }
+    }
+    catch (...) {
+    }
+    //CREAR UI
+}
+
+MainMenuState::~MainMenuState() {
+    for (auto b : buttons) delete b;
+}
+
+void MainMenuState::update() {
+    GameState::update();
+}
+
+void MainMenuState::render() const {
+    GameState::render();
+    // render 
+}
+
+void MainMenuState::handleEvent(const SDL_Event& e) {
+    GameState::handleEvent(e);
+    // handle keyboard + select
+}
