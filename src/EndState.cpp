6cd6208457dac1169a9bfd58a542229321bc0195
iriadz <idocam01@ucm.es>
2025-11-30 15:27:30 +0100

    endstate

---
+++ src/EndState.cpp
@@ -0,0 +1,20 @@
+﻿#include "EndState.h"
+EndState::EndState(Game* game, bool win, const std::string& message)
+    : GameState(game), m_win(win), m_message(message) {
+}
+
+EndState::~EndState() {}
+
+void EndState::update() {
+    GameState::update();
+}
+
+void EndState::render() const {
+    GameState::render();
+    // TODO: draw end screen with m_message
+}
+
+void EndState::handleEvent(const SDL_Event& e) {
+    GameState::handleEvent(e);
+    // TODO: handle restart/return to menu/exit
+}
---
+++ src/EndState.h
@@ -0,0 +1,17 @@
+﻿
+#include "GameState.h"
+#include <string>
+
+class EndState : public GameState {
+public:
+    explicit EndState(Game* game, bool win, const std::string& message = "");
+    virtual ~EndState();
+
+    virtual void update() override;
+    virtual void render() const override;
+    virtual void handleEvent(const SDL_Event& e) override;
+
+private:
+    bool m_win;
+    std::string m_message;
+};
